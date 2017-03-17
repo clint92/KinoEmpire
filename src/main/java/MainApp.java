4d5197259399b74f56e51e8993b0d0868245ef92
sammykea <samm0091@stud.kea.dk>
2017-03-17 09:48:37 +0100

    fdsfsdf

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -4,9 +4,17 @@
  */
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
 
     public static void main(String[] args) {
         launch(args);
@@ -14,6 +22,26 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Keanu");
 
+        initRootLayout();
+
+    }
+
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/view/Start.fxml"));
+            rootLayout = loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
