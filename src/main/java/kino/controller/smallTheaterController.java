331f8b9839edfdc838063c8736e15aefd3c65a21
Darkranger <alighali92@hotmail.com>
2017-03-21 10:52:55 +0100

    cancel til small theater

--- src/main/java/kino/controller/smallTheaterController.java
+++ src/main/java/kino/controller/smallTheaterController.java
@@ -1,5 +1,7 @@
 package kino.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import kino.MainApp;
 
 /**
@@ -8,9 +10,16 @@ import kino.MainApp;
  */
 public class smallTheaterController {
 
+    @FXML
+    Button cancel;
+
     private MainApp mainApp;
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
+    }
+
+    public void onCancelClicked() {
+        this.mainApp.showCreateReservationMenu();
     }
 }
