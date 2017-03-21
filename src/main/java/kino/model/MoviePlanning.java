aa53e9b097e01b7bf4169c18af80ea3ff71d5e7b
David Holte <davidholte123@gmail.com>
2017-03-21 10:27:51 +0100

    Merge branch 'master' into david1
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ src/main/java/kino/model/MoviePlanning.java
@@ -0,0 +1,31 @@
+package kino.model;
+
+/**
+ * Created by Dave on 21/03/2017.
+ */
+public class MoviePlanning {
+
+    public void setMovieAsActive() {
+
+
+    }
+
+    public void setMovieAsInactive() {
+
+
+    }
+
+    public void getActiveMovies() {
+
+
+    }
+
+    public void getInactiveMovies() {
+
+
+    }
+
+
+}
+
+
