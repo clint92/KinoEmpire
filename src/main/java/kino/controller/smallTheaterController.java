2e7ed25b3628c529189bb7c9ee7754f9a4127070
sammykea <samm0091@stud.kea.dk>
2017-03-21 20:36:42 +0100

    reserver seat = green

--- src/main/java/kino/controller/smallTheaterController.java
+++ src/main/java/kino/controller/smallTheaterController.java
@@ -1,13 +1,24 @@
 package kino.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import kino.MainApp;
 
 public class SmallTheaterController {
 
     private MainApp mainApp;
 
+    @FXML
+    private Circle A1;
+    @FXML
+    private Circle A2;
+
     public SmallTheaterController() {
 
+    }
+    public void onA1Clicked() {
+        A1.setFill(Color.GREEN);
     }
 
     public void setMainApp(MainApp mainApp) {
