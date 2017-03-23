fbb293d0529b87fd6e677288332cb9a1cf08280f
fadh0003 <fadh0003@stud.kea.dk>
2017-03-23 13:13:25 +0100

    Merge branch 'master' into eso1
    # Conflicts:
    #	src/main/java/kino/model/ESO.java

--- src/main/java/kino/model/ESO.java
+++ src/main/java/kino/model/ESO.java
@@ -1,6 +1,8 @@
 package kino.model;
 
 
+import kino.dao.DaoTicketSale;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -13,6 +15,7 @@ public class ESO {
     TicketSale ticketSale;
     ArrayList<TicketSale> ticketSales1 = new ArrayList<>();
     ArrayList<TicketSale> ticketSales2 = new ArrayList<>();
+    DaoTicketSale daoTicketSale = new DaoTicketSale();
 
     public boolean isReserved(){
         if (ticketSale.getReserved() == 1){
@@ -29,60 +32,45 @@ public class ESO {
     }
 
 
-    public void generateTicketTheater1(int ticketNr, String movieName){
+    public ArrayList<TicketSale> generateTicketTheater1(int ticketNr1, String movieName1) {
+        ArrayList<Integer> seatNr1 = new ArrayList<>();
         Random r = new Random();
-        for (int i = 0; i < ticketNr ; i++) {
+        for (int i = 0; i < ticketNr1; i++) {
             int seat = r.nextInt(240) + 1;
-           // TicketSale ticket = new TicketSale(0, 1, 180, movieName, seat);
-        }
-
-
-
-//        for (int i = 0; i < ticketNr ; i++) {
-//
-//            int seat = r.nextInt(12) + 1;
-//            for (int j = 0; j < ticketNr ; j++) {
-//                //this.ticketSale = new TicketSale(r.nextInt(2), r.nextInt(2), 180, movieName, seat, row, 2017);
-//                //ticketSales1.add(ticketSale);
-//                //if (ticketSales1.get(j).getRow() != row || ticketSales1.get(j).getSeat() != seat){
-////                    this.ticketSale = new TicketSale(r.nextInt(2), r.nextInt(2), 180, movieName, seat, row, 2017);
-//                    while(testState(ticketSale)) {
-//                        this.ticketSale = new TicketSale(r.nextInt(2), r.nextInt(2), 180, movieName, seat, row, 2017);
-//                    }
-//                    ticketSales1.add(ticketSale);
-////                    System.out.println(ticketSales1.get(j).getReserved());
-//                //}
-//            }
-//        }
-//        return ticketSales1;
-    }
-
-    public ArrayList<TicketSale> generateTicketTheater2(int ticketNr, String movieName) {
-        Random r = new Random();
-        for (int i = 0; i < ticketNr ; i++) {
-            int row = r.nextInt(25) + 1;
-            int seat = r.nextInt(16) + 1;
-            for (int j = 0; j < ticketSales2.size() ; j++) {
-                if (ticketSales2.get(j).getRow() != row || ticketSales2.get(j).getSeat() != seat){
-           //         this.ticketSale = new TicketSale(r.nextInt(2), r.nextInt(2), 180, movieName, seat, row, 2017);
-                    ticketSales2.add(ticketSale);
-                }
+            seatNr1.add(seat);
+            TicketSale ticket = new TicketSale(0, 1, 180, movieName1, r.nextInt(240) + 1, "2017", 14815847);
+            if (!seatNr1.contains(ticket.getSeat())) {
+                ticketSales1.add(ticket);
+                daoTicketSale.createTicketSale(ticket);
+            } else {
+                ticketNr1++;
             }
         }
+        return ticketSales1;
+    }
+
+    public ArrayList<TicketSale> getTicketSales1() {
+        return ticketSales1;
+    }
+
+        public ArrayList<TicketSale> generateTicketTheater2(int ticketNr2, String movieName2) {
+            ArrayList<Integer> seatNr2 = new ArrayList<>();
+            Random r = new Random();
+            for (int i = 0; i < ticketNr2 ; i++) {
+                int seat = r.nextInt(400) + 1;
+                seatNr2.add(seat);
+                TicketSale ticket = new TicketSale(0, 1, 180, movieName2, r.nextInt(400) + 1, "2017", 14815847);
+                if (!seatNr2.contains(ticket.getSeat())) {
+                    ticketSales2.add(ticket);
+                    daoTicketSale.createTicketSale(ticket);
+                } else {
+                    ticketNr2++;
+                }
+            }
         return ticketSales2;
     }
 
-    public boolean testState(TicketSale ticket){
-       if(ticket.getReserved() == ticket.getSold() ){
-           return true;
-       }
-       return false;
-    }
-
-    @Override
-    public String toString() {
-        return "ESO{" +
-                "ticketSales1=" + ticketSales1 +
-                '}';
+    public ArrayList<TicketSale> getTicketSales2() {
+        return ticketSales2;
     }
 }
