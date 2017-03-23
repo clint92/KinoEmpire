272d8f2c379b0e9f5a8a144675c761468c2d4989
Darkranger <alighali92@hotmail.com>
2017-03-23 10:04:14 +0100

    hej

--- src/main/java/kino/controller/smallTheaterController.java
+++ src/main/java/kino/controller/smallTheaterController.java
@@ -13,6 +13,12 @@ public class SmallTheaterController {
     private Circle A1;
     @FXML
     private Circle A2;
+    @FXML
+    private Circle A3;
+    @FXML
+    private Circle B1;
+    @FXML
+    private Circle C1;
 
     public SmallTheaterController() {
 
@@ -20,6 +26,14 @@ public class SmallTheaterController {
 
     public void onA1Clicked() {
         A1.setFill(Color.GREEN);
+        A1.setFill(Color.RED);
+        A1.setFill(Color.BLUE);
+    }
+    public void onB1Clicked() {
+        B1.setFill(Color.GREEN);
+    }
+    public void onC1Clicked() {
+        C1.setFill(Color.GREEN);
     }
 
     public void setMainApp(MainApp mainApp) {
